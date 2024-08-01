1798495b5c76e0fdeee2dfa16c5f4f76f1e7e980
ArthurOrlov <81583856+ArthurOrlov@users.noreply.github.com>
2024-08-01 12:00:23 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -22,21 +22,20 @@ document.addEventListener('DOMContentLoaded', function() {
       skewHack = 0,
       delta = 0;
 
-  // If the parent has .skewed class, set the skewHack var.
   if (parent.className.indexOf('skewed') != -1) {
       skewHack = 1000;
   }
 
   parent.addEventListener('mousemove', function(event) {
-      // Get the delta between the mouse position and center point.
       delta = (event.clientX - window.innerWidth / 2) * 0.5;
-
-      // Move the handle.
       handle.style.left = event.clientX + delta + 'px';
-
-      // Adjust the top panel width.
       topPanel.style.width = event.clientX + skewHack + delta + 'px';
   });
+  parent.addEventListener('touchmove', function(event1) {
+    delta = (event1.clientY - window.innerWidth / 2) * 0.5;
+    handle.style.left = event1.clientY + delta + 'px';
+    topPanel.style.width = event1.clientY + skewHack + delta + 'px';
+});
 });
 
 // Утилиты
